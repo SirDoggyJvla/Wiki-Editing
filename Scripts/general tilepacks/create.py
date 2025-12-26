a409bab70beffc59aab62c8511c0f611de0bbb0d
SimKDT <simon.cadet79@hotmail.fr>
2025-12-27 00:49:48 +0100

    tile pack pages

--- Scripts/general tilepacks/create.py
+++ Scripts/general tilepacks/create.py
@@ -27,15 +27,18 @@ class Tilepack:
     params = {}
     DEFAULT_NAME = "Tile pack ({NAME})"
     DEFAULT_EXTENSION = 'jpeg'
+    DEFAULT_GALLERY = '{{Clear}}'
+    GALLERY_TEMPLATE = "\n== Gallery ==\n<gallery>\n{files}\n</gallery>\n"
     THUMBNAIL_TEMPLATE = "{NAME} - thumbnail.{EXT}"
 
     def __init__(self, entry):
         params = {
             'NAME': self.DEFAULT_NAME.format(NAME=entry['NAME']),
             'WORKSHOPID': entry['WORKSHOPID'],
-            "VERSION": entry['VERSION'],
+            'VERSION': entry['VERSION'],
+            'GALLERY': self.DEFAULT_GALLERY,
         }
-        print(params["NAME"])
+        print("GENERATING: " + params['NAME'])
         self.params = params
         self.get_images(entry.get('IMAGE_FILES', {}))
 
@@ -49,11 +52,29 @@ class Tilepack:
             file_name = self.THUMBNAIL_TEMPLATE.format(NAME=self.params['NAME'], EXT=ext)
             images.append(file_name)
 
+        if IMAGE_TYPES.PREVIEW in image_definitions:
+            previews = image_definitions[IMAGE_TYPES.PREVIEW]
+            preview_count = previews.get("count", 0)
+            assert preview_count > 0, "Preview count must be greater than 0"
+            if preview_count > 1:
+                gallery = []
+                for i in range(1, preview_count + 1):
+                    ext = self.get_extension(previews)
+                    file_name = f"File:{self.params['NAME']} - preview{i}.{ext}"
+                    gallery.append(file_name)
+                gallery_str = "\n".join(gallery)
+                self.params['GALLERY'] = self.GALLERY_TEMPLATE.format(files=gallery_str)
+            else:
+                ext = self.get_extension(previews)
+                file_name = f"{self.params['NAME']} - preview.{ext}"
+                images.append(file_name)
+
         if len(images) > 0:
             images_str = "\n    {{Infobox/image\n"
             for idx, img in enumerate(images):
-                images_str += f"    | {idx + 1} = [[File:{img}]]\n"
-                images_str += f"    | p{idx + 1} = [[File:{img}|link=]]\n"
+                images_str += f"    | p{idx + 1} = [[File:{img}]]\n"
+            for idx, img in enumerate(images):
+                images_str += f"    | {idx + 1} = [[File:{img}|link=]]\n"
             images_str += "    }}\n"
             self.params['IMAGES'] = images_str
         else:
