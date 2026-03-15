ef7f17937179c919371db8c70c2f2556a2333529
SimKDT <simon.cadet79@hotmail.fr>
2026-03-15 14:03:57 +0100

    Translation changes

--- Scripts/generateTranslationTable.py
+++ Scripts/generateTranslationTable.py
@@ -1,41 +1,40 @@
 import os, yaml, pyperclip, json
 
-DATA_PATH = os.path.join("pz-scripts-data", "data")
-TRANSLATION_DATA_PATH = os.path.join(DATA_PATH, "translation_files")
+DATA_PATH = os.path.join("pz-translation-data", "data")
+TRANSLATION_DATA_PATH = os.path.join(DATA_PATH, "translationFiles.json")
+
+with open(TRANSLATION_DATA_PATH, "r") as f:
+    translation_data = json.load(f)
+
+translation_list = []
+for translation_type, data in translation_data.items():
+    translation_list.append((translation_type, data))
+
+translation_list.sort(key=lambda x: x[0])
 
 txt = ""
-for filename in os.listdir(TRANSLATION_DATA_PATH):
-    if filename.endswith(".yaml"):
-        with open(os.path.join(TRANSLATION_DATA_PATH, filename), "r") as f:
-            data = yaml.safe_load(f)
+for translation_type, data in translation_list:
+    keyPrefix = data.get("keyPrefix", None)
+    if keyPrefix is not None:
+        keyPrefix = f"<code>{keyPrefix}</code>"
+    else:
+        keyPrefix = ""
 
-            keyPrefix = data.get("keyPrefix", None)
-            if keyPrefix is not None:
-                keyPrefix = f"<code>{keyPrefix}</code>"
-            else:
-                keyPrefix = ""
+    function = data.get("function", None)
+    if function is not None:
+        function = f"<code>{function}</code>"
+    else:
+        function = ""
 
-            function = data.get("function", None)
-            if function is not None:
-                function = f"<code>{function}</code>"
-            else:
-                function = ""
+    filename = data.get("fileName", None)
+    if filename is not None:
+        filename = f"<code>{filename}</code>"
+    else:
+        filename = ""
 
-            description = data.get("description", "")
-            
-            txt += f"|-\n| {data['fileName']} || {keyPrefix} || {function} ||\n{description}\n"
+    description = data.get("description", "")
+
+    txt += f"|-\n| {translation_type} || {filename} || {keyPrefix} || {function} ||\n{description}\n"
 
 pyperclip.copy(txt)
-
-input("Press enter to continue")
-
-LANGUAGE_DATA_PATH = os.path.join(DATA_PATH, "languageCodes.json")
-
-with open(LANGUAGE_DATA_PATH, "r") as f:
-    language_data = json.load(f)
-
-txt = ""
-for language in language_data:
-    encoding = language.get("encoding", "UTF-8")
-    txt 
-
+print(txt)
